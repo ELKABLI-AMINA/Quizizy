fa11de7b67bf9cffe34831985c801adf3f0d000d
amina <amina.elkabli1@gmail.com>
2023-01-05 14:13:57 +0100

    validation

--- assets/js/home-name.js
+++ assets/js/home-name.js
@@ -1,4 +1,23 @@
 function setname(){
+
   let firstname =document.getElementById("name").value;
   localStorage.setItem("textvalue",firstname);
 }
+function validateName(){
+
+let name = document.getElementById('name');
+let button = document.getElementById('checkbtn');
+regName = /^([\w]{5})+$/;
+if(name.value==''){
+    console.log("disabled")
+}else {
+  if(!regName.test(name.value)){ 
+    button.removeAttribute("disabled")
+    console.log(name)
+  }else {
+     button.setAttribute("disabled","disabled");
+  }
+}
+
+}
+
